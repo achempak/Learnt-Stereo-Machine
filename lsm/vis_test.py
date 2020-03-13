dfb40a0f6bbda8b400f6b0dc2ed54e3e4bc56563
Aditya Chempakasseril <adityac@live.com>
2020-03-12 21:02:36 -0700

    vis tools

---
+++ lsm/vis_test.py
@@ -0,0 +1,30 @@
+import torch
+from vis_tools import *
+from shapenet_pytorch import ShapeNetDataset
+from IPython.display import display
+from IPython.core.display import HTML
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+vox_dir =  os.path.join(sys.path[0], 'shapenet_sample/voxels/modelVoxels64')
+im_dir = os.path.join(sys.path[0], 'shapenet_sample/renders')
+split_file = os.path.join(sys.path[0], 'shapenet_sample/splits_sample.json')
+
+categories = ['bench']
+dataset = ShapeNetDataset(im_dir, vox_dir, 4, 64, split_file, train=False, categories=categories)
+imgs, vol, K, R = dataset[0]
+print("imgs shape" + str(imgs.shape))
+print("imgs type" + str(type(imgs)))
+vol = vol.squeeze()
+pts, cols = voxel2pts(vol)
+display(plot_points(pts, cols, size=0.4, axis=False, title=None, html_out='test.html'))
+plot_images(imgs, 'test.png')
+
+# Center outputs
+HTML("""
+<style>
+.output {
+    align-items: center;
+}
+</style>
+""")
---
+++ lsm/vis_tools.py
@@ -0,0 +1,92 @@
+import os
+import sys
+from pathlib import Path
+
+import numpy as np
+import torch
+from IPython.display import IFrame
+from matplotlib import pyplot as plt
+from scipy.io import loadmat
+from scipy.ndimage.filters import median_filter
+from uuid import uuid4
+
+with open(os.path.join(sys.path[0], 'pyntcloud.js'), 'r') as f:
+    TEMPLATE_POINTS = f.read()
+
+
+def array_to_color(array, cmap="Oranges"):
+    s_m = plt.cm.ScalarMappable(cmap=cmap)
+    return s_m.to_rgba(array, norm=False)[:, :-1]
+
+def plot_points(xyz, colors=None, size=0.1, axis=False, title=None, html_out=None):
+    positions = xyz.reshape(-1).tolist()
+
+    Path('vis/preds').mkdir(parents=True, exist_ok=True)
+    if html_out is None:
+        html_out = os.path.join('vis/preds/', str(uuid4())+'.html')
+    else:
+        html_out = os.path.join('vis/preds/', html_out)
+    if title is None:
+        title = "PointCloud"
+    camera_position = xyz.max(0) + abs(xyz.max(0))
+    look = xyz.mean(axis=0)
+
+    if colors is None:
+        colors = [1, 0.5, 0] * len(positions)
+    elif len(colors.shape) > 1:
+        colors = colors.reshape(-1).tolist()
+    
+    if axis:
+        axis_size = xyz.ptp() * 1.5
+    else:
+        axis_size = 0
+    
+    with open(html_out, "w") as html:
+        html.write(
+            TEMPLATE_POINTS.format(
+                title=title,
+                camera_x=camera_position[0],
+                camera_y=camera_position[1],
+                camera_z=camera_position[2],
+                look_x=look[0],
+                look_y=look[1],
+                look_z=look[2],
+                positions=positions,
+                colors=colors,
+                points_size=size,
+                axis_size=axis_size
+            )
+        )
+
+    return IFrame(html_out, width=1024, height=768)
+
+def plot_images(imgs, out_file=None):
+    Path('vis/imgs').mkdir(parents=True, exist_ok=True)
+    a,b,c,d = imgs.shape
+    imgs = imgs.reshape((a,c,d,b))
+    a,b,c,d = imgs.shape
+    num_views = a if a <= 4 else 4
+    fig, axes = plt.subplots(2,2)
+    for i in range(num_views):
+        if(i < 2):
+            axes[0][i].imshow(imgs[i,int(b/3)*2:int(b/3)*3,int(b/3)*2:int(b/3)*3,:])
+        else:
+            axes[1][i-2].imshow(imgs[i,int(b/3)*2:int(b/3)*3,int(b/3)*2:int(b/3)*3,:])
+    if out_file is None:
+        out_file = os.path.join('vis/imgs/',str(uuid4())+'.html')
+    else:
+        out_file = os.path.join('vis/imgs/',out_file)
+    plt.savefig(out_file)
+#def voxel_grid(voxels, thresh=0.4, cmap='viridis'):
+
+
+
+def voxel2pts(voxels, thresh=0.4, cmap='Oranges'):
+    if voxels.ndim == 4:
+        voxels = voxels.squeeze()
+    elif voxels.ndim != 3:
+        print('Invalid number of dimensions in voxel grid')
+    vox = (voxels > thresh).astype(np.int)
+    points = np.argwhere(vox > 0)
+    colors = array_to_color(voxels[vox > 0], cmap=cmap)
+    return points, colors
