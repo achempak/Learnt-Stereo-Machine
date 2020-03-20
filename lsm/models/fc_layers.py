6655541a38fcd0077c5ba081624021e541480d13
Aditya Chempakasseril <adityac@live.com>
2020-03-19 17:31:11 -0700

    fixed bugs with dimensions

--- lsm/models/fc_layers.py
+++ lsm/models/fc_layers.py
@@ -8,18 +8,20 @@ class FCLayers(nn.Module):
         self.in_channels = in_channels
         self.dim = dim
 
-        self.deconv_1 = nn.ConvTranspose3d(in_channels=in_channels, out_channels=1, kernel_size=3)
-        self.fc_1 = nn.Linear(in_features=dim**3, out_features=dim**3)
-        #self.fc_2 = nn.Linear(in_features=dim**3, out_features=dim**3)
+        self.deconv_1 = nn.ConvTranspose3d(in_channels=in_channels, out_channels=1, kernel_size=3, padding=1)
+        self.fc_1 = nn.Linear(in_features=dim, out_features=dim)
+        self.fc_2 = nn.Linear(in_features=dim, out_features=dim)
 
         self.deconv_bn1 = nn.BatchNorm3d(1)
-        #self.fc_bn1 = nn.BatchNorm3d(1)
+        self.fc_bn1 = nn.BatchNorm3d(1)
 
     def forward(self, fused_grid):
+        #print(fused_grid.shape)
         x_deconv_1 = self.deconv_1(fused_grid)
-        out = self.fc_1(self.deconv_bn1(F.relu(x_deconv_1)))
-        #out = self.fc_2(self.fc_bn1(F.relu(x_fc_1)))
-        out = out.view((-1, 1, self.dim, self.dim, self.dim))
+        #print(x_deconv_1.shape)
+        x_fc_1 = self.fc_1(self.deconv_bn1(F.relu(x_deconv_1)))
+        #print(x_fc_1.shape)
+        out = self.fc_2(self.fc_bn1(F.relu(x_fc_1)))
+        #print(out.shape)
         out = torch.sigmoid(out)
-        print(out.shape)
         return out
